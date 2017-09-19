ddfd8882293f70ec0316a143e8c83e8c5eb22920
kaisinli <kaisinli12@gmail.com>
2017-09-19 18:55:11 -0400

    Big O

--- permutation.js
+++ permutation.js
@@ -11,26 +11,39 @@ should be in alphabetical order.
 
 // permutations problems will tend to be factorial time and space complexity
 
-function stringPermutations(str) {
-    //str is sorted 
-    if (str.length === 1) return [str];
-    const all = [];
-    let i = 0;
+// finds all possible permutations *while* maintaining the order of the characters
+function stringPermutations (str) {
+  if (str.length === 1) return [str]; // base case
+  const all = [];
 
-    while (i < str.length) {
-        const letter = str[i];
-        const allOtherChars = str.slice(0, i) + str.slice(i + 1);
+  // go through each character in the string
+  let i = 0;
+  while (i < str.length) {
 
-        stringPermutations(allOtherChars).forEach(submpermut => {
-            all.push(letter + submpermut);
-        });
+    // get each individual character
+    const letter = str[i];
 
-        while (str[i] === letter) i++;
-    }
-    return all;
+    // get all the other characters
+    const otherChars = str.slice(0, i) + str.slice(i + 1);
+
+    // compute all permutations of the *other* characters
+    stringPermutations(otherChars).forEach(submpermut => {
+      // add the current letter to the front of each of these "sub-permutations"
+      // include *that* into the full result set
+      all.push(letter + submpermut);
+    });
+
+    // increment until we reach a new letter (to avoid duplicates in the result set), there are can duplicate letter next to each other due to the fact that it is sorted
+    while (str[i] === letter) i++;
+  }
+
+  return all;
 }
 
-function sortedStringPermutations(str) {
-    const sortedStr = str.split('').sort().join('');
-    return stringPermutations(sortedStr);
+
+function sortedStringPermutations (str) {
+  // first sort the characters in the string
+  const sortedStr = str.split('').sort().join('');
+  // then find the ordered permutations of that sorted string
+  return stringPermutations(sortedStr);
 }
