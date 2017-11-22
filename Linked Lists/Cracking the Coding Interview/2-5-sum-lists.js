4d70d56bd6c360106a9d4379b705582f944194c5
kaisinli <kaixinli91@gmail.com>
2017-11-22 01:50:46 -0500

    s

---
+++ Linked Lists/Cracking the Coding Interview/2-5-sum-lists.js
@@ -0,0 +1,8 @@
+/**
+ * Input: (7 -> 1 -> 6) + (5 -> 9 -> 2) This is 617+292
+ * Output: 2 -> 1 -> 9, 912
+ */
+
+function sumList (l1, l2){
+    
+}
--- Linked Lists/reverse-linked-list.js
+++ Linked Lists/reverse-linked-list.js
@@ -4,12 +4,14 @@ function reverseList(node) {
     if (node === null || node.next === null) return node;
     var prev = null;
     var cur = node;
+    var temp;
     while (cur !== null) {
-        var temp = cur;
+        temp = cur;
         cur = cur.next;
         temp.next = prev;
         prev = temp;
     }
+
     return prev;
 }
 
@@ -19,12 +21,24 @@ function reverseList(node) {
  * 
  * 0>1>2>3>4 ==> 4>3>2>1>0
  * ------------------------
- * What we want to do here is basically to flip the direction of the LL. 
- * In other words, whatever the .next of a node is is its .prev. Since
- * this is a singly LL, there is no .prev so we need to keep track of the
- * node's previous node with temp
  *    
- */
+ *   after x iteration         prev      cur        temp     
+ *          0                  null     01234       null
+ *          1                     0        1234      0
+ *          2                    10        234       10
+ *          3                   210        34        210
+ *          4                  3210         4        3210
+ *          5                 43210      null        43210
+ *
+ * 
+ * Steps: 
+ * 1. make a copy of current node
+ * 2. move node of interest (cur) to the next one: cur = cur.next;
+ * 3. now, working with the copy (temp), set the head of the copy to point to prev:  temp.next = prev;
+ * 4. prev is temp, which we will need to reference in the next iteration
+ * 
+ * 
+ * /
 
 // ------------------------------------------------------------------------------------------------------------------------
 
