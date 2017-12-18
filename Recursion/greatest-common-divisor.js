f4548d063cba7ba4033da4bf6fc89f4d50c5e26c
kaisinli <kaixinli91@gmail.com>
2017-12-18 00:16:43 -0500

    moar

--- Recursion/greatest-common-divisor.js
+++ Recursion/greatest-common-divisor.js
@@ -1,3 +1,33 @@
-const gcd = (nums) => {
+// const gcd = (...nums) => {
+//     let answer = Math.min.apply(null, nums);
+//     while(answer > 0) {
+//         for (let i = 0; i < nums.length; i++) {
+//             if (nums[i] % answer !== 0) {
+//                 answer--;
+//                 break;
+//             } else if (i === (nums.length-1)){
+//                 return answer;
+//             } 
+//         }
+//     }
+// }
 
+const gcd = (...nums) => {
+    let answer = Math.min.apply(null, nums);
+    let answerFound = false;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % answer !== 0) {
+            break;
+        } else if (i === (nums.length - 1)) {
+            answerFound = true;
+        }
+    }
+
+    if (answerFound) {
+        return answer;
+    } else {
+      let newNums = nums.map(num => num - 1);
+      return gcd(...newNums);
+    }
 }
