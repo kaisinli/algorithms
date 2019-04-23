445c5300f72e321d28e21f1bc3f788dafd0f128c
kaisinli <kaixinli91@gmail.com>
2019-04-22 22:48:44 -0400

    moar

--- Recursion/search.js
+++ Recursion/search.js
@@ -35,7 +35,24 @@ function binarySearchRecursive(array, target, low, high) {
   if (array[mid] < target) {
     return binarySearchRecursive(array, target, mid + 1, high);
   }
-  
+
+  return mid;
+}
+
+// no recursion
+function binarySearch(array, target) {
+  let low = 0,
+    high = array.length - 1,
+
+  if (array[mid] === target) return mid;
+
+  while (low <= high) {
+    let mid = low + (Math.floor((end - start) / 2))
+    if (array[mid] < target) low = mid + 1
+    else if (array[mid] = target) return mid
+    else high = mid - 1
+  }
+
   return mid;
 }
 
