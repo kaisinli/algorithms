9909853117692637f73ae8919e521435ffdd2601
kaisinli <kaixinli91@gmail.com>
2019-05-31 01:28:03 -0400

    moar

--- Recursion/fibonacci.js
+++ Recursion/fibonacci.js
@@ -37,11 +37,11 @@ const fibonacci = (n) => {
 
 
 // the following returns the number given the position in the sequence
-function fib(n){
-    if (n === 0) return 0;
-    if (n === 1) return 1;
-    return (fib(n-1)+fib(n-2))
-}
+// function fib(n){
+//     if (n === 0) return 0;
+//     if (n === 1) return 1;
+//     return (fib(n-1)+fib(n-2))
+// }
 
 const memoFib = function() {
 	let memo = {};
